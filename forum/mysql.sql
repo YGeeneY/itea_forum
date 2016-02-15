edc90bd514859ca4cf9fc2a306371f52b1e4f303
YGeeneY <eyalansky@gmail.com>
2016-02-15 15:14:41 +0200

    fix whats wrong

--- forum/mysql.sql
+++
@@ -1,2 +0,0 @@
-create database forum CHARACTER SET utf8 COLLATE utf8_general_ci;
-grant all on forum.* to forum_user@localhost identified by '1';
--- forum/postgresql.sql
+++
@@ -1,3 +0,0 @@
-create database forum ENCODING 'UTF8' LC_COLLATE = 'uk_UA.UTF-8' LC_CTYPE = 'uk_UA.UTF-8';
-create user forum_user password '1';
-grant all privileges on database forum to forum_user;
